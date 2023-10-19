8e90554bf61f0f6e9b860b5280dc1581dda6f235
Jean Paulino <jeanpaulino.ssp@gmail.com>
2023-10-19 12:20:13 -0300

    att

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className={`flex h-screen justify-center items-center bg-gradient-to-r from-purple-500`}>
+      <p className="text-4xl">Hello World!</p>
+    </div>
+  );
+}
